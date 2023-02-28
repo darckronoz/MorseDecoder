2ec87e72897062a27942a82708fd5e7ad3e927e8
darckronoz <crispipe14@gmail.com>
2023-02-28 01:56:31 -0500

    audio encoder finished

--- main.js
+++ main.js
@@ -3,12 +3,15 @@ const msgInput = document.querySelector('#msgInput');
 const morseInput = document.querySelector('#morseInput');
 const toStringBtn = document.querySelector('#convertToString');
 const toMorseBtn = document.querySelector('#convertToMorse');
+const audioMsgInput = document.querySelector('#audio-msgInput');
+const toAudioMorseBtn = document.querySelector('#toAudioMorseBtn');
 
 class MorseDecoder {
-    constructor(msg, morse, code) {
+    constructor(msg, morse, code, codeAudio) {
         this.msg = msg;
         this.code = code;
         this.morse = morse;
+        this.codeAudio = codeAudio;
     }
 
     setMorse(morse) {
@@ -38,20 +41,56 @@ class MorseDecoder {
         });
         return result;
     }
+
+    decodeAudio() {
+        let delay = 0;
+        this.msg.split('').forEach(n => {
+            setTimeout(() => {
+                this.codeAudio.get(n.toUpperCase()).play();
+            }, delay);
+            delay += 1600;
+        });
+    }
 }
 
+const codeAudioValues = []
 const code = new Map([
     ['A', '.-'], ['B', '-...'], ['C', '-.-.'], ['D', '-..'], ['E', '.'], ['F', '..-.'], ['G', '--.'], ['H', '....'],
     ['I', '..'], ['J', '.---'], ['K', '-.-'], ['L', '.-..'], ['M', '--'], ['N', '-.'], ['O', '---'], ['P', '.--.'], ['Q', '--.-'],
     ['R', '.-.'], ['S', '...'], ['T', '-'], ['U', '..-'], ['V', '...-'], ['W', '.--'], ['X', '-..-'],
-    ['Y', '-.--'], ['Z', '..--'], ['Á', '.--.-'], ['É', '..-..'], ['Ñ', '--.--'], ['Ó', '---.'],
-    [' ', '.......']
+    ['Y', '-.--'], ['Z', '..--'], [' ', '.......']
+    //, ['Á', '.--.-'], ['É', '..-..'], ['Ñ', '--.--'], ['Ó', '---.'],
+    //[' ', '.......']
 ]);
 
-const decoder = new MorseDecoder('', '', code);
+const audioCode = new Map([]);
+
+function fillAudioList() {
+    a = [...code.keys()];
+    a.forEach(letter => {
+        if(letter != ' ') {
+            codeAudioValues.push(new Audio(`assets/audio/${letter}_morse_code.ogg`));
+        }
+    });
+}
+
+function fillAudioMap() {
+    letters = [...code.keys()];
+    let i = 0
+    letters.forEach(letter => {
+        audioCode.set(letter, codeAudioValues[i]);
+        i++;
+    });
+}
+
+fillAudioList();
+fillAudioMap();
+const decoder = new MorseDecoder('', '', code, audioCode);
 
 toStringBtn.addEventListener('click', convertToString);
 toMorseBtn.addEventListener('click', convertToMorse);
+toAudioMorseBtn.addEventListener('click', convertToAudio);
+
 
 function convertToString() {
     if(morseInput.value != ' ') {
@@ -66,3 +105,11 @@ function convertToMorse() {
         morseInput.value = decoder.encode();
     }
 }
+
+function convertToAudio() {
+    if(audioMsgInput.value != ' ') {
+        decoder.setMsg(audioMsgInput.value)
+        decoder.decodeAudio();
+    }
+}
+
